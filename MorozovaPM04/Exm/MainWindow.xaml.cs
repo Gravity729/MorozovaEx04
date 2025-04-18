58a3aad700fdbfb684b4e3918654a190a8d11bf4
Maria <164921750+Gravity729@users.noreply.github.com>
2025-04-18 12:02:45 +0300

    2Commit

---
+++ MorozovaPM04/Exm/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Exm.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+
+namespace Exm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadOrders();
+        }
+
+        private void LoadOrders()
+        {
+            using (var db = new MorozovaPM04Context())
+            {
+                OrdersList.ItemsSource = db.OrdersAssortment
+                    .Include(oa => oa.IdAssortmentPizzaNavigation).ToList();
+            }
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(sender is Border border && border.DataContext is OrdersAssortment selectedOrders)//если в рамке есть данные тогда отркрывает редактирование
+                if (new EditWindow(selectedOrders).ShowDialog() == true)
+                    LoadOrders();
+        }
+
+        private void AddButton(object sender, RoutedEventArgs e)
+        {
+            if(new AddWindow().ShowDialog() == true)            
+                LoadOrders();            
+        }
+    }
+}
---
+++ MorozovaPM04/Exm/Models/AssortmentPizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Exm.Models
+{
+    public partial class AssortmentPizza
+    {
+        public AssortmentPizza()
+        {
+            OrdersAssortment = new HashSet<OrdersAssortment>();
+        }
+
+        public int IdAssortmentPizza { get; set; }
+        public int? IdPizza { get; set; }
+        public int? IdSizePizza { get; set; }
+        public int? IdWeightPizza { get; set; }
+        public double? Price { get; set; }
+
+        public virtual Pizza IdPizzaNavigation { get; set; }
+        public virtual SizePizza IdSizePizzaNavigation { get; set; }
+        public virtual WeightPizza IdWeightPizzaNavigation { get; set; }
+        public virtual ICollection<OrdersAssortment> OrdersAssortment { get; set; }
+    }
+}
