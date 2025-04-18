0d433c4934260b2bf23deaf9c4edf495de9f00c8
Maria <164921750+Gravity729@users.noreply.github.com>
2025-04-18 12:05:17 +0300

    рабочий проект

---
+++ Exm/Models/OrdersAssortment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Exm.Models
+{
+    public partial class OrdersAssortment
+    {
+        public int IdOrder { get; set; }
+        public int? IdAssortmentPizza { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public double? House { get; set; }
+        public string Room { get; set; }
+        public string Phone { get; set; }
+        public DateTime? DateOrder { get; set; }
+        public double? Cost { get; set; }
+
+        public virtual AssortmentPizza IdAssortmentPizzaNavigation { get; set; }
+
+        public string FullFoi => $"{Lastname} {Firstname} {Surname}";
+        public string FullAdress => $"Адрес: {Street}, {House}, {Room}";
+
+        public string SumDiscount => $"Итого: {CalculatingDiscount()}";
+
+        public double? CalculatingDiscount()
+        {
+            using(var db= new MorozovaPM04Context())
+            {
+                double? total = db.AssortmentPizza
+                    .Where(ap => ap.IdAssortmentPizza == IdAssortmentPizza)
+                    .Sum(ap => ap.Price);
+
+                if (total > 4000)
+                {
+                    double? sum = total * 0.07;
+                    return sum;
+                }
+                else if (total > 2000)
+                {
+                    double? sum = total * 0.05;
+                    return sum;
+                }
+                else
+                    return total;
+            }
+        }
+    }
+}
---
+++ Exm/Models/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Exm.Models
+{
+    public partial class Pizza
+    {
+        public Pizza()
+        {
+            AssortmentPizza = new HashSet<AssortmentPizza>();
+        }
+
+        public int IdPizza { get; set; }
+        public string NamePizza { get; set; }
+        public string Ingredients { get; set; }
+
+        public virtual ICollection<AssortmentPizza> AssortmentPizza { get; set; }
+    }
+}
